59a5135d39c87f93acc45ef0db8142e194add45a
Buza Dan <buzadandeveloper@gmail.com>
2024-07-26 15:22:16 +0300

    added calendar in UP

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+const Calendar = () => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar sx={{
+        margin: '1em',
+        '& .MuiPickersCalendarHeader-root' : {
+            backgroundColor: '#3754DB',
+            borderRadius: '12px',
+            color: 'white'
+        },
+        '& .MuiSvgIcon-root' : {
+            color: 'white'
+        },
+        '& .css-23p0if-MuiButtonBase-root-MuiPickersDay-root:not(.Mui-selected)' : {
+            backgroundColor: "#3754DB",
+            border: 'none',
+            color: 'white'
+        }
+      }}/>
+    </LocalizationProvider>
+  );
+};
+export default Calendar;
--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./UserProfile.css";
 import { useAuth } from "../Context/AuthContext";
+import Calendar from "../Calendar/Calendar";
+import image from "../../assets/task-icon/task-icon.png";
 
 const UserProfile = () => {
-  const {logout} = useAuth();
+  const { logout } = useAuth();
   return (
     <div className="main-container-up">
+      <div className="edit-up">
+          <button>Edit</button>
+          <button onClick={logout}>Log Out</button>
+        </div>
       <div className="content-up">
         <div className="up-img">
-          <img src="" alt="" />
+          <img src={image} alt="" />
         </div>
         <div className="up-details">
-            <p className="name">Name</p>
-            <p className="email">Email</p>
-            <button onClick={logout}>Log Out</button>
+          <p className="name">Buza Dan</p>
+          <p className="email">buzadanfx@gmail.com</p>
         </div>
+        <Calendar/>
       </div>
     </div>
   );
