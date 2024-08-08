10fa1cb5a48fe783bc080462175571c3fd79ffaf
Buza Dan <buzadandeveloper@gmail.com>
2024-08-08 21:57:29 +0300

    added header in Task View Component with style

---
+++ src/components/TaskViewHeader/TaskViewHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TaskViewHeader.css";
+import Badge from "../Badget/Badge";
+
+const TaskViewHeader = ({ id, status, openTaskInfo, updateTaskStatus }) => {
+  return (
+    <div className="task-header">
+      <p className="task-id">{id}</p>
+      <Badge
+        status={status}
+        taskId={id}
+        openTaskInfo={openTaskInfo}
+        updateTaskStatus={updateTaskStatus}
+      />
+    </div>
+  );
+};
+
+export default TaskViewHeader;
