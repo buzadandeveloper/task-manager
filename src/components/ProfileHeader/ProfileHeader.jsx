6beca8d2d64e5836bceae19b391d32a38e10f2b9
Buza Dan <buzadandeveloper@gmail.com>
2024-08-19 13:45:58 +0300

    feat: add close button

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProfileHeader.css";
+import { useAuth } from "../Context/AuthContext";
+import { IoIosClose } from "react-icons/io";
 
-function ProfileHeader({
+const ProfileHeader = ({
   editUserProfile,
   handleSave,
   handleCancel,
   handleEdit,
   changePassword,
   logout
-}) {
+}) => {
+  const { toggleProfile } = useAuth();
   return (
     <div className="head-up">
       {editUserProfile ? (
@@ -25,17 +28,22 @@ function ProfileHeader({
           )}
         </>
       ) : (
-        <>
-          <button className="edit-btn" onClick={handleEdit}>
-            Edit
-          </button>
-          <button className="logout-btn" onClick={logout}>
-            Log Out
-          </button>
-        </>
+        <div className="edits-btn">
+          <div className="btns-head-up">
+            <button className="edit-btn" onClick={handleEdit}>
+              Edit
+            </button>
+            <button className="logout-btn" onClick={logout}>
+              Log Out
+            </button>
+          </div>
+          <div className="close-button-up">
+            <IoIosClose size={30} onClick={toggleProfile} />
+          </div>
+        </div>
       )}
     </div>
   );
-}
+};
 
 export default ProfileHeader;
