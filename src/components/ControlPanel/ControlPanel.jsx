50c85ffa2053beb31586ce0b0f867173647c6b76
Buza Dan <buzadandeveloper@gmail.com>
2024-08-19 13:05:09 +0300

    feat: add profile btn

--- src/components/ControlPanel/ControlPanel.jsx
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -3,17 +3,26 @@ import "./ControlPanel.css";
 import CreateButton from "../CreateTaskButton/CreateButton";
 import FilterStatus from "../FilterStatus/FilterStatus";
 import { TaskContext } from "../Context/TaskContext";
+import { useAuth } from "../Context/AuthContext";
 
 function ControlPanel() {
   const { openModal, taskList, setFilter, filteredLength } =
     useContext(TaskContext);
-
+  const { user, toggleProfile } = useAuth();
   return (
     <div className="control-panel-wrapper">
       <div className="control-panel-container">
         <div className="cp-up">
           <h3>Task Manager</h3>
-          {taskList.length > 0 && <CreateButton openModal={openModal} />}
+          <div className="buttons-pr-cr">
+            <img
+              className="profile-btn-img"
+              src={user.image}
+              alt="profile-image"
+              onClick={toggleProfile}
+            />
+            {taskList.length > 0 && <CreateButton openModal={openModal} />}
+          </div>
         </div>
         <FilterStatus setFilter={setFilter} filteredLength={filteredLength} />
       </div>
