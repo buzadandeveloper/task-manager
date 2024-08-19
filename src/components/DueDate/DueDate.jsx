42f44b3bc2cfa73f2e14e4a1101eef4087e2655b
Buza Dan <buzadandeveloper@gmail.com>
2024-08-19 13:40:49 +0300

    fix: invalid date

---
+++ src/components/DueDate/DueDate.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./DueDate.css";
+
+function DueDate({ dueDate }) {
+  let date;
+  try {
+    date = new Date(dueDate);
+    if (isNaN(date.getTime())) {
+      throw new Error("Invalid Date");
+    }
+  } catch (error) {
+    console.error("Invalid date:", dueDate);
+  }
+
+  return <span className="date-txt">{date.toLocaleDateString()}</span>;
+}
+
+export default DueDate;
